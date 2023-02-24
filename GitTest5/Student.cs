23890c9ab701bacc0627c02db111a111c141b09e
Elvin1999 <elvincamalzade00@gmail.com>
2023-02-24 19:57:19 +0400

    i add properties to Student class

--- GitTest5/Student.cs
+++ GitTest5/Student.cs
@@ -9,5 +9,7 @@ namespace GitTest5
     public class Student
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
     }
 }
