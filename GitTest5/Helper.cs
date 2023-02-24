388a6cc45fdca051af21764835821b5fa1d88737
Elvin1999 <elvincamalzade00@gmail.com>
2023-02-24 20:17:15 +0400

    i add helper class and some properties

---
+++ GitTest5/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest5
+{
+    public class Helper
+    {
+        public static decimal GetData()
+        {
+            return 100M;
+        }
+    }
+}
